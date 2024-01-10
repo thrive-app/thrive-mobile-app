12f6719cbf66c095a5fd486f6724dcc764242a2b
akshay-p-123 <akshay.pespunuri@gmail.com>
2024-01-09 19:46:01 -0600

    Fixed issue w/ fonts not loading on emulator and updated gitignore.

--- App.js
+++ App.js
@@ -5,16 +5,32 @@ import { tokenCache } from "./tokenCache";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { CLERK_PUBLISHABLE_KEY } from "@env";
 import BetterStatusBar from "./components/StatusBar";
-
+import * as SplashScreen from 'expo-splash-screen'
+import { useFonts } from 'expo-font'
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSansRegular: require("./assets/fonts/DMSans-Regular.ttf"),
+    DMSansMedium: require("./assets/fonts/DMSans-Medium.ttf"),
+    DMSansBold: require("./assets/fonts/DMSans-Bold.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <ThemeProvider>
       <ClerkProvider
         publishableKey={CLERK_PUBLISHABLE_KEY}
         tokenCache={tokenCache}
       >
-        <SafeAreaProvider>
+        <SafeAreaProvider onLayout={onLayoutRootView}>
           <BetterStatusBar />
           <Navigation />
         </SafeAreaProvider>
